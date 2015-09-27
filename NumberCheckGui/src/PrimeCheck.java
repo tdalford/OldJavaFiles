b0031c380c73789c9803748a6445384aae1bf86c
Tommy Alford <tdalford1@gmail.com>
2015-09-27 16:09:07 -0400

    simple prime checker

---
+++ NumberCheckGui/src/PrimeCheck.java
@@ -0,0 +1,80 @@
+import java.awt.*;
+
+import BreezyGUI.*;
+
+public class PrimeCheck extends GBFrame
+{ 
+	private Label firstLabel; 
+   	private Label answerLabel;
+	
+	private IntegerField firstField; 
+   	private TextArea answerField;
+   	
+   	private Button checkButton;
+   	private Button clearButton;
+   	
+public PrimeCheck ()
+{
+		firstLabel = addLabel("    test number",1,1,1,1);
+		answerLabel = addLabel("    answer",2,1,1,1);		
+		
+		firstField = addIntegerField(0,1,2,1,1);
+		answerField = addTextArea("", 0, 2, 3, 2);
+		
+		checkButton = addButton("check primality",3,2,1,1);
+		clearButton = addButton("clear",3,1,1,1);
+}
+
+public void buttonClicked (Button buttonObj)
+	{
+  		int firstNumber = firstField.getNumber();
+  		
+  		if(buttonObj == clearButton)
+  		{
+  			firstField.setNumber(0);
+  			answerField.setText("");
+  		}
+  		
+  		else if (buttonObj == checkButton) 
+  		{
+  			if (isPrime(firstNumber))
+  					{
+  				answerField.append(firstNumber + " is prime!" + "\n");  				
+  					}
+  			else 
+  			{
+  				answerField.append(firstNumber + " isn't prime!" + "\n");
+  			}
+  			
+  		}
+
+	}
+public boolean isPrime (int testNumb)
+{
+	 if (testNumb <= 1) {
+         return false;
+     }
+     if (testNumb == 2) {
+         return true;
+     }
+     if (testNumb % 2 == 0) {
+         return false;
+     }
+     for (int i = 3; i <= Math.sqrt(testNumb) + 1; i += 2) {
+         if (testNumb % i == 0) {
+             return false;
+         }
+     }
+     return true;
+ }
+
+public static void main (String[] args)
+{
+	//Instantiate the GUI part
+	Frame frm = new PrimeCheck();    
+	//Set the application's window width and height in pixels
+	frm.setSize (500, 400);  
+	//Make the window visible to the user
+	frm.setVisible (true);           
+}
+}
