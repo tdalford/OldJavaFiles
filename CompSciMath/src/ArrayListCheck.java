dfc317200948b7c7c480ce7370b7430a1d0c2956
tdalford1 <tdalford1@gmail.com>
2015-10-23 12:29:25 -0400

    nothing much

---
+++ CompSciMath/src/ArrayListCheck.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+public class ArrayListCheck {
+	public static void main(String[] args)
+	{
+		ArrayList <String> names = new ArrayList <String>();
+		names.add("Lucas");
+		names.add("Simon");
+		names.add("John");
+		names.add("Tommy");
+		names.add("Sana");
+		names.add("Delaney");
+		names.add("Ellie");
+		names.add("Janelle");
+		System.out.println(names);
+		
+		names.add("Megan");
+		System.out.println(names);
+		
+		names.remove(3);
+		System.out.println(names);
+		
+		names.add("Leah");
+		System.out.println(names);
+		
+		names.add(2, "Ryan");
+		System.out.println(names);
+		
+		names.set(2, "Katherine");
+		System.out.println(names);
+		
+		for (int a = names.size() - 2; a >= 0; a--) 
+		{
+			names.add(names.get(a));
+			names.remove(a);
+		}
+		System.out.println(names);
+		
+		
+	}
+
+}
