f6245561c6279e7e6b8d7e50dda5175f14c5305b
tdalford1 <tdalford1@gmail.com>
2016-02-12 12:26:14 -0500

    radical2

---
+++ CompSciMath/src/RandomPi.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Random;
+public class RandomPi 
+{
+	public static void main(String[] args)
+	{
+		/*double numPoints = 1000;
+		double pointsInCircle = 0;
+		for (int i = 0; i < numPoints; i++)
+		{
+			double x = Math.random();
+			double y = Math.random();
+			if (x*x + y*y <= 1)
+				{
+				pointsInCircle++;	
+				}
+		}
+		System.out.println(pointsInCircle / numPoints*4);
+		*/
+		Random rn = new Random();
+		double lowerBound = 1;
+		double upperBound = 2;
+		double minDist = 1;	
+		for (int i = 0; i < 100000; i++)
+		{
+			double test = lowerBound + (upperBound - lowerBound) * rn.nextDouble();
+			double approx = test*test;			
+			double distance = Math.abs(2 - approx);
+			if (distance < minDist && approx < 2)
+			{
+				minDist = distance;
+				lowerBound = test;
+			}
+			else if (distance < minDist && approx > 2)
+			{
+				minDist = distance;
+				upperBound = test;
+			}
+		}
+		System.out.println(lowerBound);
+		System.out.println(upperBound);
+		
+	}
+		
+		
+}
