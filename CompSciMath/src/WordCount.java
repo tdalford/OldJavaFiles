1ff7f00688c903f42b21eb09c741b23f327bf801
tdalford1 <tdalford1@gmail.com>
2016-03-08 12:25:41 -0500

    Nailed it

--- CompSciMath/src/WordCount.java
+++ CompSciMath/src/WordCount.java
@@ -4,7 +4,7 @@ public class WordCount
 {
 	public static void main(String[] args)
 	{
-		EasyReader infile = new EasyReader("testMessage.txt");
+		EasyReader infile = new EasyReader("sampletext.txt");
 		ArrayList<String> message = new ArrayList<String>();
 		while(!infile.eof())
 		{
@@ -12,6 +12,12 @@ public class WordCount
 		}
 		message.remove(message.size()-1);  // removes end of file character
 		System.out.println(message);
+		System.out.println("unique words = " + uniqueWords(message));
+		System.out.println("amount of times 'the' appears = " + wordOccuranceCount(message, "the"));
+	}
+	
+	public static int uniqueWords(ArrayList<String> message)
+	{
 		int uniqueWords = 0;
 		ArrayList<String> usedWords = new ArrayList<String>();
 		for (int i = 0; i < message.size(); i++)
@@ -33,15 +39,84 @@ public class WordCount
 					{
 					break;
 					}
-				}
-				if (usedWords.contains(word.toLowerCase()) == false)
+				}		
+				while (word.charAt(0) == '"' || word.charAt(0) == '‘')
 				{
-					usedWords.add(word);
+					word = word.substring(1, word.length());
+					if (word.isEmpty())
+					{
+						break;
+					}
+				}
+				
+				if (word.isEmpty() == false)
+				{
+					while (word.charAt(word.length() - 1) == '.' || word.charAt(word.length() - 1) == ':' 
+						|| word.charAt(word.length() - 1) == ';' || word.charAt(word.length() - 1) == ',' 
+						|| word.charAt(word.length() - 1) == '!' || word.charAt(word.length() - 1) == '"' 
+						|| word.charAt(word.length() - 1) == '’' || word.charAt(word.length() - 1) == '–')
+				{
+					word = word.substring(0, word.length() - 1);
+					if (word.isEmpty())
+					{
+						break;
+					}
+				}
+				}
+				if (usedWords.contains(word.toLowerCase()) == false && word.isEmpty() == false)
+				{
+					usedWords.add(word.toLowerCase());
 					uniqueWords++;
 				}
 			}
 		}
-		System.out.println("unique words = " + uniqueWords);
 		System.out.println(usedWords);
+		return uniqueWords;
+	}
+	
+	public static int wordOccuranceCount(ArrayList<String> message, String wordCheck)
+	{
+		int count = 0;
+		for (int i = 0; i < message.size(); i++)
+		{
+			String line = message.get(i);
+			for (int j = 0; j < line.length(); j++)
+			{
+				while (line.charAt(j) == ' ' && j < line.length() - 1)
+				{
+					j++;
+				}
+				String word = "";
+				
+				while (line.charAt(j) != ' ' && j < line.length())
+				{
+					word += line.charAt(j);
+					j++;
+					if (j == line.length())
+					{
+					break;
+					}
+				}
+				if (word.isEmpty() == false)
+				{
+					while (word.charAt(word.length() - 1) == '.' || word.charAt(word.length() - 1) == ':' 
+						|| word.charAt(word.length() - 1) == ';' || word.charAt(word.length() - 1) == ',' 
+						|| word.charAt(word.length() - 1) == '!' || word.charAt(word.length() - 1) == '"' 
+						|| word.charAt(word.length() - 1) == '’' || word.charAt(word.length() - 1) == '–')
+				{
+					word = word.substring(0, word.length() - 1);
+					if (word.isEmpty())
+					{
+						break;
+					}
+				}
+				}
+				if (word.toLowerCase().equals(wordCheck))
+				{
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 }
