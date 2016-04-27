75c81d4ec939815f99c358541ade4888fac925af
tdalford1 <tdalford1@gmail.com>
2016-04-27 12:33:10 -0400

    Working on problem

--- CompSciMath/src/TicTacMethods.java
+++ CompSciMath/src/TicTacMethods.java
@@ -55,6 +55,97 @@ public class TicTacMethods {
 		return false;
 	}
 	
+	public static boolean oWins(char[][] board)
+	{
+		//   012
+		// 0 XXX
+		// 1 XXX
+		// 2 XXX
+	
+		
+		if (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')
+		{
+			return true;
+		}
+		
+		
+		else if (board[2][0] == 'O' && board[2][1] == 'O' && board[2][2] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[2][0] == 'O' && board[1][1] == 'O' && board[0][2] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[1][0] == 'O' && board[1][1] == 'O' && board[1][2] == 'O')
+		{
+			return true;
+		}
+		
+		else if (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void xNextMove()
+	{
+		for (int r = 0; r < 3; r++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				
+			}
+		}
+	}
+	
+	public static boolean xTurn(char[][] board)
+	{
+		int xNum = 0;
+		int oNum = 0;
+		for (int r = 0; r < 3; r++)
+		{
+			for (int c = 0; c < 3; c++)
+			{
+				if (board[r][c] == 'X')
+				{
+					xNum++;
+				}
+				
+				else if (board[r][c] == 'O')
+				{
+					oNum++;
+				}
+			}
+		}
+		
+		if (xNum == oNum)
+		{
+			return true;
+		}
+		
+		return false;
+	}
+	
 	public static void main(String[] args)
 	{
 		char[][] board = new char[3][3];
@@ -66,7 +157,13 @@ public class TicTacMethods {
 			}		
 		}
 		
-		System.out.println(xWins(board));
+		System.out.println(oWins(board));
+		System.out.println(xTurn(board));
+		
+	}
+	
+	public boolean isEmpty(char[][] board)
+	{
 		
 	}
 }
---
+++ CompSciMath/src/WrapAroundCode.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class WrapAroundCode {
+	
+	static char[] alphabet = new char[26];
+	
+	public static void main(String[] args)
+	{
+		//add characters to AlphabetList
+		for (int i= 65; i < 91; i++)
+		{
+			alphabet[i - 65] = (char) i;
+		}
+		
+		System.out.println(getValue('Z'));
+		System.out.println(getChar(-26));
+		Scanner myScanner = new Scanner(System.in);
+		System.out.println("Input values to encode:");
+		String values = myScanner.nextLine();
+		for (int i = 0; i < values.length(); i++)
+		{
+			char value = values.charAt(i);
+			if (65 <= value && value <= 90)
+			{
+				for (int j = i + 1; j < values.length(); j++)
+				{
+					if (48 <= values.charAt(j) && values.charAt(j) <= 58)
+					{
+						int numb = values.charAt(j);			
+					}
+				}
+			}
+				
+		}
+		
+		
+	}
+	
+	public static int getValue(char letter)
+	{
+		int value = (int) letter - 64;
+		return value;
+	}
+	
+	public static char getChar(int value)
+	{
+		value %= 26;
+		int ascii = 89 - value;
+		if (ascii > 90)
+		{
+			ascii = 180 - ascii;
+		}
+		return (char) ascii;
+	}
+	
+	public char change(char letter, int numb)
+	{
+		int value = getValue(letter);
+		if (numb == 1)
+		{
+			value *= 2;
+		}
+		else if (numb == 2)
+		{
+			value %= 3;
+			value *= 5;
+		}
+		else if (numb == 3)
+		{
+			value /= 4;
+			value *= -8;
+		}
+		else if (numb == 4)
+		{
+			value = ((int) Math.floor(Math.sqrt((double) value))) * -12;
+		}
+		else if (numb == 5)
+		{
+			value = factorSum(value)*10;
+		}
+		
+		return getChar(value);
+	}
+	
+	public int factorSum(int numb)
+	{
+		int sum = 0;
+		for (int i = 1; i < numb / 2; i++)
+		{
+			if (numb % i == 0)
+			{
+				sum += i;
+			}
+		}
+		return sum;
+	}
+}
