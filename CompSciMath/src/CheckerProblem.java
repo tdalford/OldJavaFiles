38d0e2d0b9e8b5e342485b8a3b31392665131b27
tdalford1 <tdalford1@gmail.com>
2016-05-02 12:25:32 -0400

    Better

--- CompSciMath/src/CheckerProblem.java
+++ CompSciMath/src/CheckerProblem.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class CheckerProblem {
 	static char[][] checkerBoard = new char[8][8];
+	static int youAmt;
+	static int oppAmt;
+	static int[][] youPos;
+	static ArrayList<Integer> oppPos;
 
 	public static void main(String[] args) 
 	{
@@ -16,22 +21,67 @@ public class CheckerProblem {
 				checkerBoard[i][j] = '-';
 			}
 		}
-		String checkerPos = "2, 1, 3, 1, 5, 6, 2, 4, 2, 6, 4, 2, 4, 6, 6, 6, 4, 4";
+		String checkerPos = "1, 1, 5, 2, 2, 6, 4, 6";
 		initializeBoard(checkerPos);
-		for (int i =7; i >= 0; i--)
+		for (int i = 7; i >= 0; i--)
 		{
-			for (int j = 0; j < 7; j++)
+			for (int j = 0; j < 8; j++)
 			{
 				System.out.print(checkerBoard[i][j]);
 			}
 			System.out.println();
 		}
+		
+		for (int i = 0; i < youAmt; i++)
+		{
+			for (int j = 0; j < 2; j++)
+			{
+				System.out.println(youPos[i][j]);
+			}
+		}
+		
+		int maxJumps = 0;
+		for (int i = 0; i < youPos.length; i++)
+		{
+			int jumps = 0;
+			int rCoord = youPos[i][0] - 1;
+			int cCoord = youPos[i][1] - 1;
+			boolean canJump = true;
+			while (canJump)
+			{					
+				if (checkerBoard[rCoord + 1][cCoord + 1] == 'O')
+				{
+					jumps++;
+					rCoord+=2;
+					cCoord+=2;
+				}
+				else if (checkerBoard[rCoord + 1][cCoord - 1] == 'O')
+				{
+					jumps++;
+					cCoord-=2;
+					rCoord+=2;
+				}
+				else
+				{
+					canJump = false;
+				}
+			}
+			if (jumps > maxJumps)
+			{
+				maxJumps = jumps;
+			}
+		}
+		System.out.println("max jumps = " + maxJumps);
+		
+		
+		
+		
 	}
 
 	public static void initializeBoard(String checkerPos)
 	{
-		int youAmt = Character.getNumericValue(checkerPos.charAt(0));
-		int oppAmt;
+		youAmt = Character.getNumericValue(checkerPos.charAt(0));
+		youPos = new int[youAmt][2];
 		int checkerNum = 0;
 		int i = 2;
 		//1, 1, 5, 2, 2, 6, 4, 6
@@ -52,6 +102,8 @@ public class CheckerProblem {
 			checkerBoard[xCoord - 1][yCoord - 1] = 'Y';
 			System.out.print(xCoord + ", ");
 			System.out.println(yCoord);
+			youPos[checkerNum][0] = xCoord;
+			youPos[checkerNum][1] = yCoord;
 			checkerNum++;
 		}
 
@@ -82,4 +134,5 @@ public class CheckerProblem {
 			checkerNum++;
 		}
 	}
+
 }
