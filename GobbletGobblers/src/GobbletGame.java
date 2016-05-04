c3ab176c2dae381b936ff3fcd5ad5089d70ffb78
tdalford1 <tdalford1@gmail.com>
2016-05-04 12:26:27 -0400

    Copied a GUI and starting to shift it to work with Gobbler

--- GobbletGobblers/src/GobbletGame.java
+++ GobbletGobblers/src/GobbletGame.java
@@ -1,30 +1,135 @@
-
-public class GobbletGame 
+import java.util.Scanner;
+import java.util.ArrayList;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class GobbletGame extends JFrame implements ActionListener
 {
 	static Gobbler[][][] board = new Gobbler[3][3][3];
 	static int[] blueGobbAmt = new int[3];
 	static int[] redGobbAmt = new int[3];
+	JPanel panel;
+	JButton[] button;
+	int count = 0;
+	int sign = 0;
+	public GobbletGame() 
+	{
+		panel = new JPanel();
+		panel.setLayout(new GridLayout(3,3));
+		this.add(panel);
+		button = new JButton[9];
+		for(int i = 0; i <= 8; i++)
+		{
+			button[i] = new JButton();
+			panel.add(button[i]);
+			button[i].setEnabled(true);
+			button[i].addActionListener(this);
+		}
+		this.pack();
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e){
+		count++;
+		for(int i =0; i <=8; i++){
+			if(button[i] == e.getSource()){
+				if(sign%2 == 0){
+					button[i].setText("X");
+					button[i].setEnabled(false);
+				}
+				else{
+					button[i].setText("O");
+					button[i].setEnabled(false);
+				}
+			
+			}
+		
+		}
+		checkWinner();
+		if(count >= 9){
+			JOptionPane.showMessageDialog(null, "Cat's Game!");
+			for(int j = 0; j<= 8; j++){
+				button[j].setText("");
+				button[j].setEnabled(true);
+			}
+			count = 0;
+			sign = 0;
+			return;
+		}
+		sign++;
+		
+	}
+	public boolean checkWinner()
+	{
+		
+	}
 	
 	public static void main(String[] args)
+	{
+		new GobbletGame();
+		run();
+	}
+	
+	public static void run()
 	{
 		//initialize Gobbler amounts
 		for (int i = 0; i < 3; i++)
 		{
-		blueGobbAmt[i] = 2;
-		redGobbAmt[i] = 2;
+			blueGobbAmt[i] = 2;
+			redGobbAmt[i] = 2;
 		}
-		printGobblerBoard(board);
-		Gobbler blueSmall = new Gobbler("Blue", 0);
-		move(blueSmall, 0, 0);
-		printGobblerBoard(board);
-		Gobbler redMid = new Gobbler("Red", 1);
-		move(redMid, 0, 0);
-		printGobblerBoard(board);
-		Gobbler blueLarge = new Gobbler("Blue", 2);
-		move(blueLarge, 0, 0);
-		printGobblerBoard(board);
-		move(blueLarge, 0, 0, 1, 1);
-		printGobblerBoard(board);
+		boolean hasWon = false;
+		boolean isBlue = true;
+		System.out.println("input a position to move a Gobbler, or input 'end' to break");
+		System.out.println();
+		System.out.println("Ex. 2, 0, 0 moves a Gobbler of size 2 to position (0, 0)");
+		System.out.println("Ex. 2, 0, 0, 1, 1 moves a Gobbler of size 2 from position (0, 0) to position (1, 1)");
+		while (hasWon == false)
+		{
+			Scanner myScanner = new Scanner(System.in);
+			String movePos = myScanner.nextLine();
+			if (movePos.indexOf("end") >= 0)
+			{
+				break;
+			} 
+			ArrayList<Integer> moveParams = new ArrayList<Integer>();
+			for (int i = 0; i < movePos.length(); i++)
+			{
+				if (48 <= movePos.charAt(i) && movePos.charAt(i) <= 58)
+				{
+					moveParams.add(Character.getNumericValue(movePos.charAt(i)));
+				}
+			}
+			String color;
+			if (isBlue)
+			{
+				color = "Blue";
+			}
+			else
+				color = "Red";
+			if (moveParams.size() == 3)
+			{
+				move(new Gobbler(color, moveParams.get(0)), moveParams.get(1), moveParams.get(2));
+			}
+			else if (moveParams.size() == 5)
+			{
+				move(new Gobbler(color, moveParams.get(0)), moveParams.get(1), moveParams.get(2), moveParams.get(3), moveParams.get(4));
+			}
+			else
+			{
+				System.out.println("Your string isn't in the correct format");
+			}
+			printGobblerBoard(board);
+			if (isBlue == true)
+			{
+				isBlue = false;
+			}
+			else
+			{
+				isBlue = true;
+			}
+			
+		}	
 	}
 	
 	//moves by taking a gobbler on the board and moving it somewhere else
@@ -32,7 +137,6 @@ public class GobbletGame
 	{
 		//move to location (x, y).  if gobbSize > currentSize you play over it, 
 		//otherwise you can't move
-		boolean canMove = true;
 		int startIndex = getTopIndex(startX, startY);
 		if (board[startX][startY][startIndex].equals(gobb))
 		{
@@ -40,23 +144,23 @@ public class GobbletGame
 		}
 		else
 		{
-			canMove = false;
 			System.out.println("illegal move!");
+			return;
 		}
 		
 		int endIndex = getTopIndex(endX, endY);
-		if (endIndex == -1 && canMove)
+		if (endIndex == -1)
 		{
 			board[endX][endY][endIndex + 1] = gobb;			
 		}
-		else if (board[endX][endY][endIndex].size() < gobb.size() && canMove)
+		else if (board[endX][endY][endIndex].size() < gobb.size())
 		{
 			board[endX][endY][endIndex + 1] = gobb;
 		}
 		else
 		{
-			canMove = false;
 			System.out.println("illegal move!");
+			return;
 		}
 			
 	}
@@ -64,7 +168,6 @@ public class GobbletGame
 	//moves using a gobbler not on the board
 	public static void move(Gobbler gobb, int endX, int endY)
 	{
-		boolean canMove = true;
 		if (gobb.color() == "Red")
 		{
 			if (redGobbAmt[gobb.size()] > 0)
@@ -72,7 +175,10 @@ public class GobbletGame
 					redGobbAmt[gobb.size()]--;
 			}
 			else
-				canMove = false;
+			{
+				System.out.println("illegal move!");
+				return;
+			}
 		}
 		else
 		{
@@ -81,22 +187,25 @@ public class GobbletGame
 				blueGobbAmt[gobb.size()]--;
 			}
 			else
-				canMove = false;	
+			{
+				System.out.println("illegal move!");
+				return;
+			}
 		}
 		
 		int endIndex = getTopIndex(endX, endY);
-		if (endIndex == -1 && canMove)
+		if (endIndex == -1)
 		{
 			board[endX][endY][endIndex + 1] = gobb;
 		}
-		else if (board[endX][endY][endIndex].size() < gobb.size() && canMove)
+		else if (board[endX][endY][endIndex].size() < gobb.size())
 		{
 			board[endX][endY][endIndex + 1] = gobb;
 		}
 		else
 		{
-			canMove = false;
-			System.out.println(" this illegal move!");
+			System.out.println("illegal move!");
+			return;
 		}
 		
 	}
